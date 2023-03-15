4d077b91f05ec239170727caae22242b0a1d2645
ColoroGit <tomasconcha31@gmail.com>
2023-03-15 19:21:27 -0300

    Se agregan ejercicios 3, 4 y 5

---
+++ labsemana1_ejercicio3/Ex03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+int barra = 0;
+int time = 0;
+int i = 0;
+Random r = new Random();
+
+while (i < 20)
+{
+    Console.Clear();
+
+    barra = r.Next(0, 4);
+    time = r.Next(2000);
+
+    i += barra;
+
+    for (var j = 0; j < i; j++)
+    {
+        System.Console.Write("/");
+    }
+    
+    Thread.Sleep(1000);
+}
